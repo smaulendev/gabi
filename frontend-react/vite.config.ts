0226bf4648806aeeea30f2b30c45457c9fa4a3f8
Sebastian Maulen <smaulen.dev@gmail.com>
2026-07-01 12:03:04 -0400

    Optimizar build con code splitting

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/react-router-dom")) {
+            return "router";
+          }
+
+          if (id.includes("node_modules/recharts")) {
+            return "charts";
+          }
+
+          if (id.includes("node_modules/lucide-react")) {
+            return "icons";
+          }
+
+          if (id.includes("node_modules/axios")) {
+            return "axios";
+          }
+
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom")
+          ) {
+            return "react";
+          }
+
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
