c0c5bb339f906f4ccb9cfa81c5126551ff01a8be
Sebastian Maulen <smaulen.dev@gmail.com>
2026-06-30 23:20:25 -0400

    Configurando API para produccion

--- frontend-react/src/api/api.ts
+++ frontend-react/src/api/api.ts
@@ -1,7 +1,23 @@
+// import axios from 'axios';
+
+// export const api = axios.create({
+//   baseURL: 'http://localhost:3000',
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://gabi-backend.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
