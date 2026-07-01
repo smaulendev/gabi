1cc77f1c59c4d9b3bfd705aca210fbe72da17c85
Sebastian Maulen <smaulen.dev@gmail.com>
2026-06-30 22:55:33 -0400

    Configuracion para despliegue con Neon

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -19,11 +19,16 @@ import { AuditModule } from './audit/audit.module';
 
     TypeOrmModule.forRoot({
       type: 'postgres',
+
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
 
       autoLoadEntities: true,
 
